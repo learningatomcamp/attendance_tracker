15c3d31c8da0c75319f0f2a5d1dd457f8f99e7cb
learningatomcamp <learning@atomcamp.com>
2025-01-20 12:15:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,7 @@ if st.session_state.logged_in:
     def main():
         # available files (can be changed later)
         menu = ["AI", "DS", "ML", "DA Gray", "DA Black", "DA White", "DS6", "DS7 Blue", "DS7 Green", "DS8 Tracker",
-                "App Development", "AI 03 Tracker", "DA Cohort 02 Orange", "DA Cohort 02 Yellow","DS Cohort 09 Electron","DS Cohort 09 proton","LRP Fellowship_attendence_Tracker"]
+                "App Development", "AI 03 Tracker", "DA Cohort 02 Orange", "DA Cohort 02 Yellow","DS Cohort 09 Electron","DS Cohort 09 proton","LRP Fellowship_attendence_Tracker","DA03 Tracker Azure","DA03 Tracker AWS"]
 
         col1.subheader("Attendance")
 
